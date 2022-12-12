4bad945b335d9cbb0775cd026bdb2cac8afc8c39
Roncollins MM <muriukiron@gmail.com>
2022-12-12 13:04:18 +0300

    create basemodel class

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+BaseClass Module - Contains implementation for the Base Class supercalss.
+"""
+
+import uuid
+import datetime
+
+class BaseModel():
+    """BaseModel class implementation.
+
+    Defines all the attributes and methods common to all child classes.
+    """
+
+    def __init__(self):
+        """BaseModel constructor."""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.last_updated = self.created_at
+
+    def __str__(self):
+        """Print this object"""
+
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__})'
+
+    def save(self):
+        """Save the instance"""
+
+        self.last_updated = datetime.datetime.now()
+
+    def to_dict(self):
+        """Convert this object to dict"""
+
+        obj_as_dict = dict(self.__dict__)
+
+        obj_as_dict.update({'__class__' : f'{self.__class__.__name__}'})
+        obj_as_dict['created_at'] = self.created_at.isoformat()
+        obj_as_dict['last_updated'] = self.last_updated.isoformat()
+
+        return obj_as_dict
