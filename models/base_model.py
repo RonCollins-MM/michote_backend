4db51ed776dcc50475ec17dab3793aaa3a291656
Roncollins MM <muriukiron@gmail.com>
2022-12-20 08:17:48 +0300

    feat: add support for creation of objects using **args
    The create function from `cli.py` can now be used to create objects using key/value pairs.
    See documentation for syntax

--- models/base_model.py
+++ models/base_model.py
@@ -21,10 +21,13 @@ class BaseModel():
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
-                if key == 'created_at' or key == 'last_updated':
-                    setattr(self, key, datetime.datetime.fromisoformat(value))
+                if key.lower() == 'id':
                     continue
                 setattr(self, key, value)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.last_updated = self.created_at
+            models.storage.new(self)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
