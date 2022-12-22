638f053a1c926d588565a2b55b211b6604080439
Roncollins MM <muriukiron@gmail.com>
2022-12-22 11:46:23 +0300

    refactor: capitalized json response for 404 Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,7 +22,7 @@ def end_session(Exception):
 @app.errorhandler(404)
 def not_found(error):
     """Function called when 404 not found error occurs"""
-    return make_response(jsonify({'error':'not found'}), 404)
+    return make_response(jsonify({'error':'Not found'}), 404)
 
 if __name__ == '__main__':
     """Launch the flask app form terminal"""
