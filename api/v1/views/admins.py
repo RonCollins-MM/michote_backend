af2e67aceca641462bd5c8545fef8e8838969dc7
Roncollins MM <muriukiron@gmail.com>
2022-12-22 19:57:12 +0300

    feat: add api routes for admin - DELETE, POST, GET
    GET: Able to get a specific Admin object using admin id
    DELETE: Able to delete a specific Admin based on Admin id
    POST: Able to create a new admin object

--- api/v1/views/admins.py
+++ api/v1/views/admins.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from models.admin import Admin
 
+admin_attributes = ['username', 'password', 'email', 'phone_number']
 
 @app_views.route('/admins', methods=['GET'], strict_slashes=False)
 def get_all_admins():
@@ -20,3 +21,40 @@ def get_all_admins():
         admins_list.append(admin_obj.to_dict())
 
     return jsonify(admins_list)
+
+@app_views.route('/admins/<admin_id>', methods=['GET'], strict_slashes=False)
+def get_one_admin(admin_id):
+    """Method called to get one Admin object based on its id"""
+    admin = storage.get(Admin, admin_id)
+
+    if not admin:
+        abort(404)
+    return jsonify(admin.to_dict())
+
+@app_views.route('/admins/<admin_id>', methods=['DELETE'], strict_slashes=False)
+def delete_admin(admin_id):
+    """Deletes a specific admin record from storage"""
+    admin = storage.get(Admin, admin_id)
+
+    if not admin:
+        abort(404)
+    storage.delete(admin)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
+
+@app_views.route('admins/signup', methods=['POST'], strict_slashes=False)
+def create_admin():
+    """Creates a new admin object"""
+    if not request.get_json():
+        abort(400, description='Not a JSON')
+    for attribute in admin_attributes:
+        if attribute not in request.get_json():
+            abort(400, description=f'Missing {attribute}')
+
+    info = request.get_json()
+    admin_obj = Admin(**info)
+    admin_obj.save()
+
+    return make_response(jsonify(admin_obj.to_dict()), 201)
+
