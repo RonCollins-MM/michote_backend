1e0c9dcdd2641357f58335f2d86f13b5a05659f1
Roncollins MM <muriukiron@gmail.com>
2022-12-22 11:07:11 +0300

    feat: add flask app module to launch the server

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""This module handles environment setup for michote API"""
+
+from flask import Flask, make_response, jsonify
+from os import getenv
+from models import storage
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+host = getenv('MICHOTE_API_HOST', '0.0.0.0')
+port = getenv('MICHOTE_API_PORT', 5000)
+
+@app.teardown_appcontext
+def end_session(Exception):
+    """Function called on session termination"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Function called when 404 not found error occurs"""
+    return make_response(jsonify({'error':'not found'}), 404)
+
+if __name__ == '__main__':
+    """Launch the flask app form terminal"""
+    app.run(host=host, port=port, threaded=True)
