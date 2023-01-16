f539cb29a100c31e16f6bb91cb3c6dd79b5acbf2
Roncollins_MM <muriukiron@gmail.com>
2023-01-16 13:43:50 +0300

    docs: update documentation for route object

--- models/route.py
+++ models/route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Contains Route class"""
+"""Contains implementation of route class"""
 
 import models
 
@@ -12,7 +12,29 @@ from os import getenv
 DATETIME_ISO = '%Y-%m-%dT%H:%M'
 
 class Route(BaseModel, Base):
-    """Implementation of Route class"""
+    """Route object definition.
+    
+    Attributes
+    ----------
+    partner_id : str
+        ID of partner company who created this route.
+    start_destination : str
+        The start destination of the route
+    end_destination : str
+         The end destination of the route
+    period_begin : datetime
+        The beginning of the period during which this route is valid for the
+        respective partner comnpany
+    period_end : datetime
+        The end of the period during which this route is valid for the
+        respective partner comnpany
+    price_per_ticket : int
+        The price for each ticket for this route
+    currency : str
+        The currency for the price per ticket
+    slots_available : int
+        The slots available for this route
+    """
     if models.storage_type == 'db':
         __tablename__ = 'routes'
         partner_id = Column(String(60), ForeignKey('partners.id'),
@@ -35,7 +57,15 @@ class Route(BaseModel, Base):
         slots_available = 0
 
     def __init__(self, *args, **kwargs):
-        """Creates new Route object"""
+        """Creates new Route object.
+        
+        Parameters
+        ----------
+        *args : key/value pairs, optional
+            Attributes passed as key value pairs passed
+        **kwargs : dict, optional
+            Dict object that contains all attributes for creating new object
+        """
         super().__init__(*args, **kwargs)
 
         if type(self.period_begin) is str:
