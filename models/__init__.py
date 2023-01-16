9448479d9942fa951e3131a7632cbbff081172e6
Roncollins_MM <muriukiron@gmail.com>
2023-01-16 12:42:15 +0300

    docs: update documentation for init module

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 
+"""
+This initialization module is responsible for creating a variable that holds
+an instance of the storage engine class.
+
+The storage type is set as an environment variable called `MICHOTE_TYPE_STORAGE`
+Two storage types exist:
+    1. File storage - Which is the default type storage when no type storage is
+        specified.
+    2. Database storage - The database in use is MySQL database.
+"""
+
 from os import getenv
 
 storage_type = getenv('MICHOTE_TYPE_STORAGE')
